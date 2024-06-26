d2fbda8a5f93c5d95c005b2292999e178b2e312e
André Hortelão <andrefhortelao@gmail.com>
2024-06-26 12:58:27 +0100

    Created components for Sidebar and Navbar, and starting organization of Dashboard page

--- fitness-tracker/src/App.jsx
+++ fitness-tracker/src/App.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 //import About from "./pages/About";
 import PageNotFound from "./pages/PageNotFound";
 import SignUp from "./pages/SignUp";
 import Logout from "./pages/Logout";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
           <Route index element={<Home/>} />
           <Route path="/home" element={<Home/>} />
           <Route path="/sign-up" element={<SignUp/>} />
+          <Route path="/dashboard" element={<Dashboard/>} />
           <Route path="*" element={<PageNotFound/>} />
           <Route path="/logout" element={<Logout/>} />
         </Routes>
