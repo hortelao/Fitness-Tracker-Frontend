ca389f5be5b8baf84cf9330e2323aa0aeede667b
André Hortelão <andrefhortelao@gmail.com>
2024-06-26 12:22:13 +0100

    Styling on 404 Page

--- fitness-tracker/src/pages/PageNotFound.jsx
+++ fitness-tracker/src/pages/PageNotFound.jsx
@@ -2,7 +2,17 @@ import React from "react";
 
 function PageNotFound() {
     return (
-        <h1>Page not found</h1>
+        <div class="page-not-found bg-light d-flex align-items-center">
+            <div class="container text-center">
+                <div class="row justify-content-center">
+                    <div class="col-md-6">
+                        <h1 class="display-1">404</h1>
+                        <p class="lead">Oops! The page you are looking for does not exist.</p>
+                        <a href="/" class="btn btn-primary">Go Home</a>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
